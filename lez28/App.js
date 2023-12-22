61a7148a0e02ebe89a5956e54b1d5be0a0fa0b1d
Luca Leopardi <luca.leopardi.98@gmail.com>
2023-12-22 15:23:57 +0100

    Finished lecture 28 ex 2 (kinda).

--- lez28/App.js
+++ lez28/App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { Component } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 import * as Location from 'expo-location';
+import MapView, { Circle, Marker } from 'react-native-maps';
 
 export default class App extends Component {
 
@@ -9,33 +11,69 @@ export default class App extends Component {
 		sid: "qWpVdN4AxqXxOvu6gX1h",
 		lat: null,
 		lon: null,
+		objects: [],
 	}
 
-	componentDidMount() {
-		this.checkLocationPermission()
+	async componentDidMount() {
+		await this.checkLocationPermission()
+		this.getLocation()
 	}
 
 	render() {
-		return (
-			<View style={styles.container}>
+		let buttons = (
+			<View>
 				<Button
 					title="Get location"
-					onPress={() => this.getLocation().then(console.log)} />
+					onPress={() => this.getLocation().then(() => console.log("Lat: " + this.state.lat + " Lon: " + this.state.lon))} />
 				<Button
 					title="Get nearby objects"
 					onPress={() => this.getObjectsNearby(
 						this.state.sid,
 						this.state.lat,
 						this.state.lon)
-						.then(console.log)} />
-				<StatusBar style="auto" />
+						.then(() => console.log(this.state.objects))} />
+			</View>
+		)
+
+		let markers = this.state.objects.map((object) => {
+			return (
+				<Marker
+					key={object.id}
+					title={object.id.toString()}
+					coordinate={{ latitude: object.lat, longitude: object.lon }}
+				/>)
+		})
+
+		let map = this.state.lat && this.state.lon ? (
+			<MapView
+				style={{ flex: 1, width: `100%`, height: `100%` }}
+				initialRegion={{
+					latitude: this.state.lat,
+					longitude: this.state.lon,
+					latitudeDelta: 0.05,
+					longitudeDelta: 0.05,
+				}}>
+				<Circle
+					center={{ latitude: this.state.lat, longitude: this.state.lon }}
+					radius={1000}
+					strokeColor={"#000"}
+					fillColor={"rgba(100, 100, 200, 0.5)"} />
+				{markers}
+			</MapView>
+		) : (null)
+
+		return (
+			<View style={styles.container}>
+				{buttons}
+				{map}
+				<StatusBar style="auto" hidden={true} />
 			</View>
 		)
 	}
 
 	async getObjectsNearby(sid) {
-		const { lat, lon } = await this.getLocation()
-		let queryParamsString = new URLSearchParams({ sid: sid, lat: lat, lon: lon }).toString();
+		await this.getLocation()
+		let queryParamsString = new URLSearchParams({ sid: sid, lat: this.state.lat, lon: this.state.lon }).toString();
 
 		let fetchData = {
 			method: "GET",
@@ -45,13 +83,13 @@ export default class App extends Component {
 		}
 		let httpResponse = await fetch("https://develop.ewlab.di.unimi.it/mc/mostri/" + "objects" + "?" + queryParamsString, fetchData);
 		const status = httpResponse.status;
-		if (status == 200) return await httpResponse.json();
-		else throw new Error("SERVER ERROR: " + status + " - " + await httpResponse.text());
+		if (status == 200) this.setState({ objects: await httpResponse.json() })
+		else throw new Error("SERVER ERROR: " + status + " - " + await httpResponse.text())
 	}
 
 	async getLocation() {
 		const location = await Location.getCurrentPositionAsync()
-		return { lat: location.coords.latitude, lon: location.coords.longitude }
+		this.setState({ lat: location.coords.latitude, lon: location.coords.longitude })
 	}
 
 	async checkLocationPermission() {
@@ -70,5 +108,6 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+		// paddingTop: Constants.statusBarHeight,	// Useless right now because StatusBar is hidden
 	},
 });
