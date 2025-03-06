1f2fea7561abc1fbd847d25a7aa7bacff0492928
Luca Leopardi | LAPTOP <63211561+LucaLeopardi@users.noreply.github.com>
2025-03-06 17:26:35 +0100

    Removed course excercises. Only final submission is left.

--- lez23/src/UserListItem.js
+++
@@ -1,9 +0,0 @@
-function UserListItem(props) {
-	return (
-		<div>
-			<h1>{props.data.name}</h1>
-			<button onClick={() => props.onUserSelect(props.data.uid)}>Details</button>
-		</div>)
-}
-
-export default UserListItem;
